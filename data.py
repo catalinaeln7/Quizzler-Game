6023e091b5f5dea161542c9127e42a04f44cb0a3
Catalina-Elena Popa <74061561+catalinaeln7@users.noreply.github.com>
2022-09-02 16:12:57 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,11 @@
+import requests
+
+parameters = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+response_json = response.json()
+question_data = response_json["results"]
